50a1cd14a14c6e7171adf3cbd3032134a71ddd0e
Re1gns <lreigns12@gmail.com>
2023-10-10 16:04:04 +0100

    correction

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-'''Task 2's module.
-'''
+
 import asyncio
 import time
 from importlib import import_module as using
 
-
 async_comprehension = using('1-async_comprehension').async_comprehension
-
 
 async def measure_runtime() -> float:
     '''Executes async_comprehension 4 times and measures the
@@ -15,4 +12,11 @@ async def measure_runtime() -> float:
     '''
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time() - 
+    return time.time() - start_time
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    total_runtime = loop.run_until_complete(measure_runtime())
+    loop.close()
+
+    print(f"Total runtime: {total_runtime:.2f} seconds")
